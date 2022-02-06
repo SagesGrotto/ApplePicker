7ba271d718458cbccfaa0bb94728b4a22f0965f7
SagesGrotto <adm7519@utulsa.edu>
2022-02-06 13:42:19 -0600

    ApplePicker and Basket Script

---
+++ Apple Picker/Assets/Scripts/Basket.cs
@@ -0,0 +1,42 @@
+/****
+* Created by: Sage
+* Date Created: Feb 4, 2022
+* 
+* Last Edited by: NA
+* Last Edited: Feb 4, 2022
+* 
+* Description: 
+****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
+}
